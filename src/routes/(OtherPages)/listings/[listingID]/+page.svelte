ba1158035803b6f930ff235387ff0ef9e865cbeb
FT111 <thetaylor2016@gmail.com>
2024-08-24 23:59:26 +0100

    Updated listing page

--- src/routes/(OtherPages)/listings/[listingID]/+page.svelte
+++ src/routes/(OtherPages)/listings/[listingID]/+page.svelte
@@ -3,6 +3,14 @@
 
 </script>
 
-<div class="mt-16">
-
+<div class="mt-16 px-8">
+	<!-- Listing Container	-->
+	<div class="p-14 w-full h-full flex flex-col">
+		<div class="flex flex-col h-[80vh] w-full">
+			<h2 class="font-bold text-6xl">Listing Title</h2>
+			
+		
+		</div>
+		
+	</div>
 </div>
