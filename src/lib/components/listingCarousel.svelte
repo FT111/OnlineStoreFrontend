f143c5e70805005577a5f019a27ae35506cc1b07
FT111 <thetaylor2016@gmail.com>
2024-09-04 20:07:00 +0100

    Added listing carousel component

---
+++ src/lib/components/listingCarousel.svelte
@@ -0,0 +1,41 @@
+<script>
+import Listing from '$lib/components/listing.svelte';
+import * as Carousel from "$lib/components/ui/carousel/index.js";
+import { fetchListings } from '$lib/api/listings.js';
+
+export let fetcher = fetchListings; // Function to fetch listings from the API
+export let title = 'Listings';
+
+</script>
+
+
+
+<div class="lg:p-20 lg:py-10 sm:p-6 flex flex-col gap-3">
+	<p class="text-4xl font-semibold">{title}</p>
+	<Carousel.Root class="w-full" opts={{
+  'dragFree': false,
+  'loop': true,
+  'slidesToScroll': 4
+
+ }}>
+		<Carousel.Content class="-ml-1">
+			{#await fetcher()}
+				{#each Array.from({ length: 10 }) as _, i}
+					<Listing isLoading={true} />
+				{/each}
+			{:then data}
+				{#each data.data as listing}
+					<Carousel.Item class="pl-1 md:basis-1/3 lg:basis-1/4">
+						<div class="p-1">
+							<Listing listingName={listing.title} listingDescription={listing.description} isLoading={false} listingPrice={listing.basePrice} />
+						</div>
+					</Carousel.Item>   {/each}
+			{:catch error}
+				<p>{error.message}</p>
+			{/await}
+		</Carousel.Content>
+		<Carousel.Previous />
+		<Carousel.Next />
+	</Carousel.Root>
+</div>
+
--- src/routes/(HomePage)/+page.svelte
+++ src/routes/(HomePage)/+page.svelte
@@ -1,12 +1,11 @@
 <script>
  import Listing from '$lib/components/listing.svelte';
+ import ListingCarousel from '$lib/components/listingCarousel.svelte';
  import SearchHero from '$lib/components/searchHero.svelte';
  import Navbar from '$lib/components/navbar.svelte';
  import { onMount } from 'svelte';
- import * as Carousel from "$lib/components/ui/carousel/index.js";
- import * as Card from "$lib/components/ui/card/index.js";
  
- import { fetchListings } from '$lib/api/listings.js';
+ import { fetchListings, queryListings } from '$lib/api/listings.js';
  import { fetchCategories } from '$lib/api/categories.js';
  
  
@@ -28,13 +27,11 @@
        navElement.classList.remove(...navHeroClasses);
        navElement.classList.add(...navPageClasses);
        
-       navSearch.classList.remove(...heroSearchClasses);
       } else {
        console.log('not intersecting');
        navElement.classList.remove(...navPageClasses);
        navElement.classList.add(...navHeroClasses);
        
-       navSearch.classList.add(...heroSearchClasses);
       }
      },
      { threshold: 0 }
@@ -67,6 +64,10 @@
  <SearchHero searchSuggestions={fetchCategories} bind:searchElement={heroSearchBar} />
 </div>
 
+
+<ListingCarousel listingsFetcher={queryListings} title='Popular' />
+<ListingCarousel listingsFetcher={queryListings} title='Computing' />
+
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 2xl:grid-cols-5 gap-3 w-full p-1 sm:p-4 md:p-8 md:px-14 justify-left">
  {#await fetchListings()}
   {#each Array.from({ length: 20 }) as _, i}
@@ -79,33 +80,5 @@
   {:catch error}
   <p>{error.message}</p>
   {/await}
-
 </div>
 
-<div class="lg:p-32 sm:p-6">
- <Carousel.Root class="w-full" opts={{
-  'dragFree': false,
-  'loop': true,
-  'slidesToScroll': 3
-
- }}>
-  <Carousel.Content class="-ml-1">
-   {#await fetchListings()}
-   {#each Array.from({ length: 10 }) as _, i}
-    <Listing isLoading={true} />
-   {/each}
-   {:then data}
-   {#each data.data as listing}
-    <Carousel.Item class="pl-1 md:basis-1/2 lg:basis-1/3">
-     <div class="p-1">
-      <Listing listingName={listing.title} listingDescription={listing.description} isLoading={false} listingPrice={listing.basePrice} />
-     </div>
-    </Carousel.Item>   {/each}
-   {:catch error}
-   <p>{error.message}</p>
-   {/await}
-  </Carousel.Content>
-  <Carousel.Previous />
-  <Carousel.Next />
- </Carousel.Root>
-</div>
