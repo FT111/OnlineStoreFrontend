6f0623efd112074566559e5df7e74a62c6dc72cd
FT111 <thetaylor2016@gmail.com>
2024-09-18 19:29:29 +0100

    Began login/signup page

---
+++ src/routes/(OtherPages)/login/+page.svelte
@@ -0,0 +1,18 @@
+
+<script>
+
+</script>
+
+<div class="flex flex-row w-full p-4 h-screen">
+<!--	Image -->
+	<div class="h-full hidden md:flex basis-1/2 p-24 rounded-3xl text-white bg-primary">
+	
+	</div>
+	
+<!--	Form -->
+	<div class="flex flex-col grow h-full items-center justify-center">
+		<h1 class="text-4xl font-bold">Welcome back!</h1>
+		<p>Please sign in to your account</p>
+	
+	</div>
+</div>
