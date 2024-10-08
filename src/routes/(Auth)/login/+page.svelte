edad88ed5baa8f59fe253aa06d39d7cf94935665
FT111 <thetaylor2016@gmail.com>
2024-10-08 23:08:18 +0100

    Minor animation polish

--- src/routes/(Auth)/login/+page.svelte
+++ src/routes/(Auth)/login/+page.svelte
@@ -1,14 +1,12 @@
 
 <script>
-  	import { baseURL } from '$lib/api/core.js';
-	import { fade } from 'svelte/transition';
+	import { fade, fly } from 'svelte/transition';
 	import {Input} from "$lib/components/ui/input"
 	import { Card } from '$lib/components/ui/card/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import { login } from '$lib/api/authentication.js';
 	import AuthButton from '$lib/components/authButton.svelte';
-	import { enhance } from '$app/forms';
-		import { goto, invalidateAll } from '$app/navigation';
+	import { backInOut } from 'svelte/easing';
 	
 		
 	let email;
@@ -53,7 +51,7 @@
 				<Input bind:value={password} type="password" required placeholder="Password" name="password"  />
 				
 				<AuthButton bind:this={submitBtnElement} authFunction={handleLoginAttempt} bind:onPress={signIn} class="w-full justify-between hover:drop-shadow-xl drop-shadow-none transition-all duration-150">
-					Sign in
+					<p out:fly={{y:-20, easing: backInOut, duration: 700}}>Sign in</p>
 					<svg out:fade xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
 						<path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
 					</svg>
