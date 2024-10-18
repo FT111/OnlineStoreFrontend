d8a084b7e196f358a63530b597687d68cea68ac1
FT111 <thetaylor2016@gmail.com>
2024-10-18 18:09:42 +0100

    Added edit button

--- src/routes/(OtherPages)/users/[userID]/+page.js
+++ src/routes/(OtherPages)/users/[userID]/+page.js
@@ -4,7 +4,6 @@ import { fetchListings } from '$lib/api/listings.js';
 
 export const load = async ({ params }) => {
 	const user = await getUserByID(params['userID']);
-	const listings = await fetchListings(params['userID']);
 	return {
 		profileUser: {
 			...user.data
--- src/routes/(OtherPages)/users/[userID]/+page.svelte
+++ src/routes/(OtherPages)/users/[userID]/+page.svelte
@@ -72,8 +72,25 @@
 					
 <!--				Action Buttons	-->
 					<div class="flex flex-row items-center justify-end h-full w-full align-middle flex-1 flex-grow gap-4">
-						<Button class="basis-1/4" variant="default">Rate</Button>
-						<Button class="basis-1/4" variant="destructive">Report</Button>
+						{#if data.user}
+							{#if data.profileUser.id === data.user.id}
+								<Button class="basis-1/4 justify-evenly" variant="default">Edit
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+										<path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+									</svg>
+								</Button>
+							{/if}
+						{/if}
+						<Button class="basis-1/4 justify-evenly" variant="default">Rate
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+							</svg>
+						</Button>
+						<Button class="basis-1/4 justify-evenly" variant="destructive">Report
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5" />
+							</svg>
+						</Button>
 						
 					</div>
 				</div>
