91a3460acffbfeaabcb757ff8aebfdcd80372479
FT111 <thetaylor2016@gmail.com>
2024-10-18 18:09:23 +0100

    Grouped user icon and rating

--- src/lib/components/listing.svelte
+++ src/lib/components/listing.svelte
@@ -32,7 +32,7 @@
 <a href="/listings/{listingID}" class="w-full h-min {className}">
 	<Card.Root class="flex flex-col justify-start h-full w-full shadow-sm group rounded-xl bg-muted/60
 					transition-all duration-150 ease-in-out hover:scale-[1.00] hover:bg-slate-200/50 hover:border-accent/50
-					border-slate-200 " href="/listings/{listingID}">
+					border-slate-200 gap-0" href="/listings/{listingID}">
 		
 		<Card.Header class="pb-4">
 			{#if isLoading === true}
@@ -46,7 +46,7 @@
 			{/if}
 		
 			<div class="flex flex-row justify-between pt-2 items-center">
-				<div class="flex flex-row gap-2 h-3 align-middle items-center p-0">
+				<div class="flex flex-row gap-2 align-middle items-center bg-slate-200 w-fit rounded-3xl p-1 pr-3">
 					<a  href={`/users/${userID}`}>
 						<Avatar.Root>
 							<Avatar.Image src={userAvatarUrl} alt='Avatar' />
@@ -55,7 +55,7 @@
 					</a>
 
 					<div class="flex flex-col">
-						<p class="text-xs font-light flex flex-row align-middle items-center gap-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+						<p class="text-xs font-light flex flex-row align-middle items-center gap-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 stroke-accent">
 							<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
 						</svg>
 							{#if isLoading === true}
