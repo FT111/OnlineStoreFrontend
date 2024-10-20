61e5bed73228a2c5ae7e51006720bf71e47c1a63
FT111 <thetaylor2016@gmail.com>
2024-10-20 11:54:45 +0100

    Added reviews and owner user icon

--- src/routes/(OtherPages)/listings/[listingID]/+page.svelte
+++ src/routes/(OtherPages)/listings/[listingID]/+page.svelte
@@ -2,8 +2,11 @@
 <script>
 	import Image from '$lib/components/image.svelte'
 	import Price from '$lib/components/price.svelte'
+	import ReviewGrid from '$lib/components/reviewGrid.svelte'
+	import ListingCarousel from '$lib/components/listingCarousel.svelte'
 	import { Button } from '$lib/components/ui/button/index.js';
 	import { Badge } from '$lib/components/ui/badge/index.js';
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
 	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
 	import { fetchListing } from '$lib/api/listings.js';
 	import { page } from '$app/stores';
@@ -144,7 +147,7 @@
 <!--Page Container-->
 <div class="mt-20 md:px-14 px-1">
 	<!-- Listing Container	-->
-	<div class="h-[80vh] bg-muted/30 rounded-2xl flex flex-row gap-5 p-5">
+	<div class="h-[80vh] bg-muted/30 rounded-2xl flex flex-col sm:flex-row gap-5 p-5">
 	
 <!--	Images Container	-->
 		<div class="basis-1/2">
@@ -152,7 +155,7 @@
 		</div>
 		
 <!--	Detail Container	-->
-		<div class="basis-1/2 w-1/2 flex flex-col gap-4 flex-shrink-0 justify-between">
+		<div class="sm:basis-1/2 basis-1 sm:w-1/2 flex flex-col gap-4 flex-shrink-0 justify-between">
 		
 <!--		Top info	-->
 			<div class="flex flex-col gap-4 flex-shrink-0 ">
@@ -181,15 +184,34 @@
 				</div>
 				
 				<!--				Listing Heading-->
-				<div class="flex flex-col gap-1 mt-4">
-
+				<div class="flex flex-col gap-1 mt-4 relative">
+					<!-- Title and description section -->
 					<h1 class="text-4xl font-bold mb-1">{listing.title}</h1>
 					<main class="line-clamp-3">{listing.description}</main>
+					
+					<!-- User section -->
+					<aside class="inline-flex flex-row gap-2 align-middle items-center bg-slate-200 w-fit  rounded-3xl p-1 pr-3 absolute right-0 top-0">
+						<a href={`/users/${listing.ownerUser.id}`}>
+							<Avatar.Root>
+								<Avatar.Image src={listing.ownerUser.profilePictureURL} alt="Avatar" />
+								<Avatar.Fallback>{listing.ownerUser.username.slice(0, 2)}</Avatar.Fallback>
+							</Avatar.Root>
+						</a>
+						
+						<div class="flex flex-col">
+							<p class="text-xs font-light flex flex-row align-middle items-center gap-1">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 stroke-accent">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+								</svg>
+								4.3
+							</p>
+						</div>
+					</aside>
 				</div>
 				
 	<!--				SKU Details -->
 				{#if skus}
-					<div class="flex flex-col bg-muted rounded-2xl overflow-x-scroll w-full border-4 border-muted">
+					<div class="flex flex-col bg-muted rounded-2xl overflow-x-scroll w-full border-4 border-slate-200">
 						<div class="flex flex-col p-2 px-4">
 							<p class="font-semibold text-lg">Colour</p>
 							<div class="flex flex-row relative rounded-t-2xl p-2 px-1 overflow-x-scroll gap-3 w-full">
@@ -201,7 +223,7 @@
 							</div>
 						</div>
 						
-						<div class="flex flex-col rounded-2xl bg-slate-50 p-2 px-4">
+						<div class="flex flex-col bg-slate-50 p-2 px-4">
 							<p class="font-semibold text-lg">Size</p>
 							<div class="flex flex-row relative rounded-t-2xl p-2 px-1 overflow-x-scroll gap-3 w-full">
 								{#each sizes as sku}
@@ -215,8 +237,8 @@
 						<div class="flex flex-col rounded-2xl p-2 px-4">
 							<p class="font-semibold text-lg">Size</p>
 							<div class="flex flex-row relative rounded-t-2xl p-2 px-1 overflow-x-scroll gap-3 w-full">
-								{#each sizes as sku}
-									<Badge class="bg-primary text-primary-foreground  hover:bg-accent w-20 p-2 px-3 rounded-3xl flex-shrink-0 transition-all duration-250 ease-in-out cursor-pointer">
+								{#each listing.skus as sku}
+									<Badge class="bg-primary text-primary-foreground  hover:bg-accent min-w-20 p-2 px-3 rounded-3xl flex-shrink-0 transition-all duration-250 ease-in-out cursor-pointer">
 										{sku.title}
 									</Badge>
 								{/each}
@@ -228,17 +250,31 @@
 			</div>
 			
 			<!--		Basket options	-->
-			<div class="w-full bottom-16 min-h-24 p-3 bg-slate-100 rounded-xl flex flex-row align-middle items-center justify-between shadow-sm z-20">
-				<div />
-				
-				<div class="flex flex-row items-center h-full gap-4">
-					<Price price={listing.basePrice} />
-					<Button class="w-full h-full text-2xl basis-1/2 font-bold border-[3px] border-amber-500 bg-primary shadow-md rounded-xl" variant="default">Add to Basket</Button>
-				</div>
-			</div>
+
 			
 		</div>
 	</div>
 	
-	
+	<div class="h-20" />
+	<!-- Reviews	-->
+	<ReviewGrid  />
+</div>
+
+<div class="sticky w-full sm:w-1/2 justify-self-end bottom-4 min-h-24 p-3 z-20 right-3 min-w-fit">
+	<div class=" bg-slate-100 rounded-xl p-3 flex flex-row align-middle items-center justify-between border-2 border-slate-200">
+		
+		<div />
+		
+		<div class="flex flex-row items-center h-full gap-4">
+			<Price price={listing.basePrice} />
+			<Button class="w-full h-full text-2xl basis-1/2 font-bold border-[3px] border-amber-500 p-3 px-5 bg-primary shadow-md rounded-xl" variant="default">Add to Basket</Button>
+		</div>
+	</div>
+</div>
+<div class="h-20" />
+<div class="bg-slate-100 ">
+	<div class="h-10"></div>
+	<ListingCarousel compactLayout={true} title="More from {listing.category}" />
+	<div class="h-20" />
+
 </div>
