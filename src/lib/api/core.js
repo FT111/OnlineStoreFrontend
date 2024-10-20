b48cb4da86f9a35131e74a2e17c5c6cc4ce9239b
FT111 <thetaylor2016@gmail.com>
2024-10-20 11:49:31 +0100

    Added analytics consent API header

--- src/lib/api/core.js
+++ src/lib/api/core.js
@@ -1,4 +1,5 @@
 import { browser } from '$app/environment';
+import { isConsentGiven } from '$lib/analytics/consent.js';
 
 export const baseURL = 'http://localhost:8000/'
 
@@ -7,12 +8,20 @@ const formHeaders = (token=null) => {
 	if (browser && !token) {
 		token = sessionStorage.getItem('token');
 	}
+	if (browser && isConsentGiven()) {
+
+	}
 
 	let headers = {'Content-Type': 'application/json',
 	'Access-Control-Allow-Origin': 'http://localhost:8000'}
 	// If token exists, add it to the headers to provide credentials to the server
 	if (token) {
-		headers['Authorization'] = `JWT ${token}`;
+		headers['Authorization'] = `Bearer ${token}`;
+	}
+	if (browser && isConsentGiven()) {
+		headers['X-Analytics-Consent'] = 'true';
+	} else {
+		headers['X-Analytics-Consent'] = 'false';
 	}
 
 	return headers;
