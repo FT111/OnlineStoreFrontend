e5bdd08a59beab7a86f53cf7e5d58ead39038bfc
FT111 <thetaylor2016@gmail.com>
2024-11-02 10:15:36 +0000

    Added more account page sidebar buttons and profile dropdown

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,13 +1,14 @@
 
 <script>
 
-let isCollapsed = false;
-
-
+	let isCollapsed = false;
+	
+	let className;
+	export { className as class };
 
 </script>
 
-<div class="h-screen sticky top-0 p-3 basis-1/6 flex-none bg-slate-100 border-r-0 border-r-slate-200">
+<div class="h-screen sticky top-0 p-4 min-w-72 max-w-72 flex-none bg-slate-100 border-r-0 border-r-slate-200 {className}">
 	<div class="sticky flex flex-col h-full w-full gap-2 pt-20">
 		<slot />
 	</div>
--- src/routes/(OtherPages)/account/+layout.svelte
+++ src/routes/(OtherPages)/account/+layout.svelte
@@ -5,6 +5,7 @@ import { Separator } from '$lib/components/ui/separator/index.js';
 import * as Avatar from '$lib/components/ui/avatar/index.js';
 import { Card } from '$lib/components/ui/card/index.js';
 import { Button } from '$lib/components/ui/button/index.js';
+import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
 
 export let data;
 $: user = data.user;
@@ -12,23 +13,53 @@ $: user = data.user;
 
 <div class="w-full h-full flex flex-row">
 	<Sidebar>
-		<div class="flex flex-col gap-4 text-slate-800">
+		<div class="flex flex-col gap-3 text-slate-800">
 <!--		User section	-->
-			<Card class="bg-slate-50 rounded-xl border-slate-200 border-2 hover:border-slate-900 transition-all shadow-none cursor-pointer p-1.5 px-2.5 flex flex-row gap-2 items-center">
-				<Avatar.Root class="text-primary h-11 w-11">
-					<Avatar.Image src={user['profilePictureURL']} alt="Avatar" />
-					<Avatar.Fallback>EB</Avatar.Fallback>
-				</Avatar.Root>
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger>
+					<Card class="bg-slate-50 rounded-xl border-slate-600 border hover:border-accent
+					transition-all shadow-none cursor-pointer p-1.5 px-2.5 flex flex-row gap-2
+					items-center justify-between ">
+						<div class="flex flex-row gap-3 items-center">
+							<Avatar.Root class="text-primary h-11 w-11">
+								<Avatar.Image src={user['profilePictureURL']} alt="Avatar" />
+								<Avatar.Fallback>EB</Avatar.Fallback>
+							</Avatar.Root>
+							
+							<div class="flex flex-col p-2 justify-start">
+								<p class="font-semibold text-xl">{user['username']}</p>
+								<p class="text-sm text-left font-light text-slate-500">{user['id']}</p>
+							</div>
+						</div>
+						
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-slate-600">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+						</svg>
+					</Card>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content class="w-56">
+					<DropdownMenu.Group>
+						<DropdownMenu.Item href="/users/{user.id}">Profile</DropdownMenu.Item>
+						<DropdownMenu.Separator />
+						<DropdownMenu.Item class="bg-red-100/50">
+							<div class="flex flex-row w-full justify-between ">Sign out
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 text-accent">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+								</svg>
+							</div>
+							</DropdownMenu.Item>
+					</DropdownMenu.Group>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
 				
-				<div class="flex flex-col p-2">
-					<p class="font-semibold text-xl">{user['username']}</p>
-					<p class="text-sm font-light text-slate-500">{user['id']}</p>
-				</div>
-			</Card>
 			
-<!--			<Separator />-->
+			<Separator />
 			
-			<Button class="bg-white hover:border-slate-900 border border-white  hover:bg-white" variant="secondary">My Account</Button>
+			
+			<Button class="bg-white hover:border-slate-900 border   hover:bg-white" variant="secondary">Dashboard</Button>
+			<Button class="bg-white hover:border-slate-900 border   hover:bg-white" variant="secondary">Listings</Button>
+			<Button class="bg-white hover:border-slate-900 border  hover:bg-white" variant="secondary">Sales</Button>
+		
 		</div>
 	</Sidebar>
 	
