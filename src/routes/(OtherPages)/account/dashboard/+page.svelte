98d4fc7e5ede952de49123820581612a8d7491a0
FT111 <thetaylor2016@gmail.com>
2024-11-04 21:10:30 +0000

    Altered grid to put less relevant stats further down

--- src/routes/(OtherPages)/account/dashboard/+page.svelte
+++ src/routes/(OtherPages)/account/dashboard/+page.svelte
@@ -1,6 +1,12 @@
 <script>
 	import DashboardPageLayout from "$lib/components/DashboardPageLayout.svelte";
+	import { ScrollArea } from "$lib/components/ui/scroll-area/index.js";
+	import ListingCarousel from "$lib/components/listingCarousel.svelte";
 	import * as Card from '$lib/components/ui/card/index.js';
+	import LatestSales from '$lib/components/dashboardCards/latestSales.svelte';
+	import { fetchListings } from '$lib/api/listings.js';
+	
+
 </script>
 
 <DashboardPageLayout>
@@ -9,23 +15,20 @@
 	<div slot="page" class="grid grid-cols-4 grid-rows-3 bg-slate-100 rounded-2xl
 	 												grow h-[42rem] gap-2.5 p-1.5">
 		
-			<Card.Root class="rounded-2xl border-0 bg-white lg:col-span-3 lg:row-span-1">
-				<Card.Header>
-					<h1 class="text-2xl font-bold text-center">Card</h1>
-				</Card.Header>
-			</Card.Root>
 		
-		<Card.Root class="rounded-2xl border-0 bg-white lg:col-span-1 lg:row-span-1">
-			<Card.Header>
-				<h1 class="text-2xl font-bold text-center">Card</h1>
-			</Card.Header>
-		</Card.Root>
+
 		
 		<Card.Root class="rounded-2xl border-0 bg-white lg:col-span-2 lg:row-span-2">
 			<Card.Header>
 				<h1 class="text-2xl font-bold text-center">Card</h1>
 			</Card.Header>
 		</Card.Root>
+		<Card.Root class="rounded-2xl border-0 bg-white lg:col-span-2 lg:row-span-1">
+			<Card.Header>
+				<h1 class="text-2xl font-bold text-center">Card</h1>
+			</Card.Header>
+		</Card.Root>
+
 		
 		<Card.Root class="rounded-2xl border-0 bg-white lg:col-span-2 lg:row-span-1">
 			<Card.Header>
@@ -33,7 +36,12 @@
 			</Card.Header>
 		</Card.Root>
 		
-		<Card.Root class="rounded-2xl border-0 bg-white lg:col-span-2 lg:row-span-1">
+		<LatestSales />
+		
+		
+
+		
+		<Card.Root class="rounded-2xl border-0 bg-white lg:col-span-1 lg:row-span-1">
 			<Card.Header>
 				<h1 class="text-2xl font-bold text-center">Card</h1>
 			</Card.Header>
