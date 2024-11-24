b86d12f90fc53d8a225c9b9bddc00721b522c592
FT111 <thetaylor2016@gmail.com>
2024-11-24 23:17:10 +0000

    Added category/subcategory dropdowns linked to backend

--- src/routes/(OtherPages)/account/listings/new/+page.svelte
+++ src/routes/(OtherPages)/account/listings/new/+page.svelte
@@ -3,6 +3,12 @@
 	import DashboardPageLayout from '$lib/components/DashboardPageLayout.svelte';
 	import * as Card from '$lib/components/ui/card/index.js';
 		import { Input } from '$lib/components/ui/input/index.js';
+		import Dropdown from '$lib/components/dropdown.svelte';
+		import { fetchCategories, fetchCategory } from '$lib/api/categories.js';
+		import { Button } from '$lib/components/ui/button/index.js';
+	
+		
+	let selectedCategory;
 </script>
 
 <DashboardPageLayout>
@@ -13,14 +19,44 @@
 			<Card.Root class="w-full sm:w-4/5 md:w-3/4 lg:w-1/2 ">
 				<Card.Header>
 					<h2 class="text-2xl font-semibold">Create a new listing</h2>
+					<p class="font-light text-slate-600">Add some basic information about your product. Pricing and images will be added at a later stage.</p>
 				</Card.Header>
 				<Card.Content>
 					<form>
-						<div class="flex flex-col gap-2">
+						<div class="flex flex-col gap-3.5 items-end">
 							<Input label="Title" placeholder="Enter a title for your listing" />
 							<Input label="Description" placeholder="Enter a description for your listing" />
+							
+							<div class="flex flex-row gap-3.5 w-full">
+								<div class="basis-1/2">
+									{#await fetchCategories()}
+									<Dropdown title="Category" />
+								{:then categories}
+									<Dropdown options={categories.data.map(category => {return category.title})} title="Category" bind:value={selectedCategory}
+									subtitle="Find a category"/>
+								{:catch error}
+									<p>{error.message}</p>
+								{/await}
+								</div>
+								
+								<div class="basis-1/2">
+								{#if selectedCategory}
+									{#key selectedCategory}
+										{#await fetchCategory(selectedCategory)}
+											<Dropdown title="Subcategory" />
+										{:then categoryData}
+											<Dropdown options={categoryData.data.subCategories.map(subcategory => {return subcategory.title})} title="Subcategory"
+											subtitle="Search for a subcategory"/>
+										{:catch error}
+											<p>{error.message}</p>
+										{/await}
+									{/key}
+								{/if}
+								</div>
+							</div>
+							
+							<Button variant="default" size="lg">Next</Button>
 						</div>
-						
 						
 					</form>
 				</Card.Content>
