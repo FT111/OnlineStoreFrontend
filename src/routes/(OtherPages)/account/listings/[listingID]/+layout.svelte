45f38c762ec48a8ffa9baf68eef5e80044cf413f
FT111 <thetaylor2016@gmail.com>
2024-12-05 09:45:44 +0000

    Added public status indicator to listing page layout

--- src/routes/(OtherPages)/account/listings/[listingID]/+layout.svelte
+++ src/routes/(OtherPages)/account/listings/[listingID]/+layout.svelte
@@ -1,12 +1,13 @@
 <script>
 
-	import { Plus } from 'lucide-svelte';
+	import { Info, Plus } from 'lucide-svelte';
 
 		export let data;
 
 	import DashboardPageLayout from '$lib/components/DashboardPageLayout.svelte';
 		import * as Card from '$lib/components/ui/card/index.js';
 	import Price from '$lib/components/price.svelte';
+		import { Badge } from '$lib/components/ui/badge/index.js';
 
 </script>
 
@@ -27,18 +28,27 @@
 <!--				Main listing card -->
 				<div class="flex-col flex gap-2.5 w-fit h-fit p-4 rounded-l-2xl bg-amber-50">
 					<p>Listing</p>
-					<Card.Root class="w-56 border-slate-700 h-36 border-[1.5px]">
-						<Card.Header class="p-3.5">
-							<p>{data.listing.title}</p>
-						</Card.Header>
-						<Card.Description class="px-3.5">
-							<p>{data.listing.description}</p>
-						</Card.Description>
+					<Card.Root class="w-56 border-slate-700 h-36 border-[1.5px] justify-between flex flex-col">
+						<div>
+							<Card.Header class="p-3.5">
+								<p>{data.listing.title}</p>
+							</Card.Header>
+							<Card.Description class="px-3.5">
+								<p>{data.listing.description}</p>
+							</Card.Description>
+						</div>
+						
+						<Card.Footer class="p-3.5 flex flex-row gap-1 w-full">
+							<Badge class="py-1.5 px-2.5 gap-1" variant={data.listing.public ? 'default' : 'secondary'}>
+								<Info class="size-4" />
+								<p>{data.listing.public ? 'Public' : 'Private'}</p>
+							</Badge>
+						</Card.Footer>
 					</Card.Root>
 				</div>
 
 				<div class="flex-col flex gap-2.5 w-fit h-fit p-4 rounded-2xl">
-					<p>Variants</p>
+					<p>Products</p>
 					<div class="flex flex-row gap-3 h-full">
 						{#each data.listing.skus as sku}
 							<Card.Root class="w-56 h-36 justify-between flex flex-col">
