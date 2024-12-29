328118c386d8bb16938566085ebccc8622cba598
FT111 <thetaylor2016@gmail.com>
2024-12-29 23:26:37 +0000

    Migration prep - Commited UI components

---
+++ src/lib/components/ui/avatar/avatar.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { Avatar as AvatarPrimitive } from "bits-ui";
+	import { cn } from "$lib/utils.js";
+	let className = undefined;
+	export let delayMs = undefined;
+	export { className as class };
+</script>
+
+<AvatarPrimitive.Root
+	{delayMs}
+	class={cn("relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full " +
+	 "outline outline-0 hover:outline-2 outline-accent transition-all duration-100 ", className)}
+	{...$$restProps}
+>
+	<slot />
+</AvatarPrimitive.Root>
---
+++ src/lib/components/ui/skeleton/skeleton.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { cn } from "$lib/utils.js";
+	let className = undefined;
+	export { className as class };
+</script>
+
+<div class={cn("bg-orange-400/60 animate-pulse rounded-md", className)} {...$$restProps}></div>
