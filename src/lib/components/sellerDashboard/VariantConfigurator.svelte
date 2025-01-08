b83bb2df76322b350a657b806d9c801aca13c230
FT111 <thetaylor2016@gmail.com>
2025-01-08 17:36:48 +0000

    Users can now add option categories to variation config

--- src/lib/components/sellerDashboard/VariantConfigurator.svelte
+++ src/lib/components/sellerDashboard/VariantConfigurator.svelte
@@ -7,8 +7,10 @@
 		import { Input } from '$lib/components/ui/input/index.js';
 		import { Cross, Plus, Trash2, X } from 'lucide-svelte';
 		import { Button } from '$lib/components/ui/button/index.js';
+	import {fly} from 'svelte/transition';
+	import {backOut} from 'svelte/easing';
 
-	
+
 	let { variantOptions = $bindable({}), selectedOptions = $bindable({}),
 		skus = {},
 		selectedProduct = $bindable({}),
@@ -115,8 +117,12 @@
 		variantOptions[category] = variantOptions[category].filter((opt) => opt !== option);
 	};
 	
-	const addCategory = (category) => {
-		variantOptions[category] = [];
+	const addCategory = (form) => {
+		form.preventDefault();
+		const formData = new FormData(form.target);
+		variantOptions[formData.get('categoryValue')] = [];
+		form.target.reset();
+		
 	};
 	
 	const removeCategory = (category) => {
@@ -129,7 +135,7 @@
 <div class="flex flex-col bg-muted rounded-2xl  w-full border-4 border-slate-200 overflow-clip">
 	
 	{#each Object.keys(variantOptions) as category}
-		<div class="flex flex-col bg-slate-50 p-2 px-4 border-b-2 border-slate-200">
+		<div in:fly={{y:50}} class="flex flex-col bg-slate-50 p-2 px-4 border-b-2 border-slate-200">
 			<div class="flex flex-row gap-1 items-center">
 				{#if configuring}
 					<Button class="px-2" variant={"ghost"}>
@@ -141,11 +147,11 @@
 				{/if}
 				
 			</div>
-			<div class="flex flex-row flex-wrap relative rounded-t-2xl p-2 px-1 gap-3 w-full">
+			<div class="flex flex-row flex-wrap relative rounded-t-2xl p-2 px-1 gap-2 w-full">
 				{#each variantOptions[category] as option}
-					<button onclick={()=>{onSelect(category, option)}} class="{selectedOptions[category]===option ? ' !bg-accent border-amber-900 ' : '' }
+					<button in:fly={{x:-100, duration: 150, z:0}} out:fly={{x:-100, duration: 150, z: 0}} onclick={()=>{onSelect(category, option)}} class="{selectedOptions[category]===option ? ' !bg-accent border-amber-900 ' : '' }
 					{currentAvailableOptions[category].includes(option)||!validation ? '' : ' disabled !hover:bg-primary/60 !bg-primary/60 !cursor-default  '}
-					 bg-primary flex flex-row gap-1 h-8 items-center font-semibold text-xs text-primary-foreground border-2 border-transparent outline-none hover:bg-destructive justify-center min-w-16 w-fit p-2 px-3 rounded-3xl flex-shrink-0 transition-all duration-250 ease-in-out cursor-pointer">
+					 bg-primary flex flex-row z-10 gap-1 h-8 items-center font-semibold text-xs text-primary-foreground border-2 border-transparent outline-none hover:bg-destructive justify-center min-w-16 w-fit p-2 px-3 rounded-3xl flex-shrink-0 transition-all duration-250 ease-in-out cursor-pointer">
 						{option}
 						{#if configuring}
 							<X class="w-4 h-4 ml-1" />
@@ -154,7 +160,7 @@
 				{/each}
 				{#if configuring}
 					<form class="flex flex-row gap-1 bg-emerald-100/60 rounded-3xl" onsubmit={(form)=>{addOption(form, category)}}>
-						<Input name="optionValue" class="min-w-20 w-fit h-8 p-2 px-3 rounded-3xl border-emerald-200 bg-emerald-50 flex-shrink-0 transition-all duration-250 ease-in-out " placeholder="Add an option" />
+						<Input minlength="1" name="optionValue" class="min-w-20 w-fit h-8 p-2 px-3 rounded-3xl border-emerald-200 bg-emerald-50 flex-shrink-0 transition-all duration-250 ease-in-out " placeholder="Add an option" />
 						<button class="px-2 rounded-3xl bg-emerald-200 text-black hover:brightness-75 transition-all" type="submit">
 							<Plus size={20} strokeWidth={1.25} />
 						</button>
@@ -169,5 +175,11 @@
 </div>
 	{/if}
 {#if configuring}
-	<Input class="min-w-20 w-fit p-2 px-3 rounded-3xl border-emerald-200 bg-emerald-50 flex-shrink-0 transition-all duration-250 ease-in-out" placeholder="+ Add a category" />
+	<form class="flex flex-row gap-1 w-fit bg-emerald-50 border-emerald-200 border border-1 rounded-3xl" onsubmit={(form)=>{addCategory(form)}}>
+		<label class="sr-only" for="categoryValue">Add an option category</label>
+		<Input minlength="1" name="categoryValue" class="min-w-32 w-48 h-10 p-2 px-3 rounded-3xl bg-emerald-50  border-none flex-shrink-0 transition-all duration-250 ease-in-out " placeholder="Add an option category" />
+		<button class="flex flex-row px-2 rounded-r-3xl bg-emerald-200 text-black items-center justify-center hover:brightness-75 transition-all size-10" type="submit">
+			<Plus size={20} strokeWidth={1.25} />
+		</button>
+	</form>
 {/if}
