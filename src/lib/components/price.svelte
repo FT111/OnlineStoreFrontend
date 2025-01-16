e6abd13737864e58f4e7c93480b39e110e522693
FT111 <thetaylor2016@gmail.com>
2025-01-16 10:45:38 +0000

    Fixed price reactivity

--- src/lib/components/price.svelte
+++ src/lib/components/price.svelte
@@ -2,18 +2,15 @@
 <script>
 	
 
-	let { class: className = undefined, price = $bindable(), discounted } = $props();
-	price = price / 100;
+	let { class: className = undefined, price, discounted } = $props();
 	
-	let priceInteger = Math.floor(price);
-	let priceDecimal = (price - priceInteger).toFixed(2).slice(2);
+	let priceInteger = $derived(Math.floor(price/100));
+	let priceDecimal = $derived((price/100 - priceInteger).toFixed(2).slice(2));
 </script>
 
 
-{#key price}
 	{#if discounted}
 		<p class={"text-4xl font-bold text-amber-800 "}>£{priceInteger}<sup class="text-xl font-medium">{priceDecimal}</sup></p>
 	{:else}
 		<p class={"text-4xl font-bold"}>£{priceInteger}<sup class="text-xl font-medium">{priceDecimal}</sup> </p>
 	{/if}
-{/key}
