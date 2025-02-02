c738eb4485ad053b36d75f2dc4d149d459cce2bf
FT111 <thetaylor2016@gmail.com>
2025-02-02 17:39:20 +0000

    Added card brand indicator to saved card

--- src/routes/(NoLayout)/checkout/+page.svelte
+++ src/routes/(NoLayout)/checkout/+page.svelte
@@ -19,6 +19,16 @@
 	let previousPage = $state('/')
 	// Stores temporarily to provide visual feedback
 	let cardNumber = $state('')
+	let cardBrand = $derived.by(() => {
+		if (cardNumber?.toString().startsWith('4')) {
+			return 'Visa'
+		} else if (cardNumber?.toString().startsWith('3')) {
+			return 'American Express'
+		} else if (cardNumber?.toString().startsWith('5') || cardNumber?.toString().startsWith(('2'))) {
+			return 'Mastercard'
+		}
+	})
+	
 	// Controls the state of the payment dialog
 	let paymentDialogOpenState = $state(false)
 	
@@ -137,7 +147,8 @@
 									<CreditCard size={28} strokeWidth={1.25} />
 									<div class="flex flex-col justify-evenly items-start transition-all">
 										<p class="text-lg font-mono">**** **** **** {transaction.paymentMethod.cardNumber.slice(-4)}</p>
-										<p class="text-sm text-muted-foreground">Expires {transaction.paymentMethod.cardExpiry}</p>
+										<p class="text-sm text-muted-foreground">
+											{cardBrand} • Expires {transaction.paymentMethod.cardExpiry}</p>
 									</div>
 								</div>
 								<Edit2 size={20} strokeWidth={1.25} class="opacity-40 group-hover:opacity-90 transition-all" />
@@ -165,10 +176,10 @@
 										</div>
 									</InputWithLabel>
 									<div class="size-24 pt-3 grid grid-cols-1 grid-rows-1 content-center place-items-center">
-									{#if cardNumber?.toString().startsWith('4')}
+									{#if cardBrand === 'Visa'}
 										<svg in:fly={{x: -50, z: -10}} out:fly={{x: -50, z: -10}} style="grid-column: 1; grid-row: 1" class="size-full  block m-auto pt-3 content-center items-center place-items-center text-center" fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M16.539 9.186a4.155 4.155 0 0 0-1.451-.251c-1.6 0-2.73.806-2.738 1.963-.01.85.803 1.329 1.418 1.613.631.292.842.476.84.737-.004.397-.504.577-.969.577-.639 0-.988-.089-1.525-.312l-.199-.093-.227 1.332c.389.162 1.09.301 1.814.313 1.701 0 2.813-.801 2.826-2.032.014-.679-.426-1.192-1.352-1.616-.563-.275-.912-.459-.912-.738 0-.247.299-.511.924-.511a2.95 2.95 0 0 1 1.213.229l.15.067.227-1.287-.039.009zm4.152-.143h-1.25c-.389 0-.682.107-.852.493l-2.404 5.446h1.701l.34-.893 2.076.002c.049.209.199.891.199.891h1.5l-1.31-5.939zm-10.642-.05h1.621l-1.014 5.942H9.037l1.012-5.944v.002zm-4.115 3.275.168.825 1.584-4.05h1.717l-2.551 5.931H5.139l-1.4-5.022a.339.339 0 0 0-.149-.199 6.948 6.948 0 0 0-1.592-.589l.022-.125h2.609c.354.014.639.125.734.503l.57 2.729v-.003zm12.757.606.646-1.662c-.008.018.133-.343.215-.566l.111.513.375 1.714H18.69v.001h.001z"></path></g></svg>
-									{:else if cardNumber?.toString().startsWith('3')}
-										<svg in:fly={{x: -50, z: -10}} out:fly={{x: -50, z: -10}} style="grid-column: 1; grid-row: 1"  class="size-full scale-[0.65] block m-auto pt-3 content-center items-center place-items-center text-center" width="256px" height="256px" viewBox="0 0 256 256" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  preserveAspectRatio="xMidYMid">
+									{:else if cardBrand === 'American Express'}
+										<svg in:fly={{x: -50, z: -10}} out:fly={{x: -50, z: -10}} style="grid-column: 1; grid-row: 1"  class="size-full scale-[0.6] block m-auto pt-3 content-center items-center place-items-center text-center" width="256px" height="256px" viewBox="0 0 256 256" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  preserveAspectRatio="xMidYMid">
 											<title>AMEX</title>
 											<g>
 												<rect fill="#FFFFFF" x="0" y="0" width="256" height="256"></rect>
@@ -176,7 +187,7 @@
 											</g>
 										</svg>
 									
-									{:else if cardNumber?.toString().startsWith('5') || cardNumber?.toString().startsWith(('2'))}
+									{:else if cardBrand === 'Mastercard'}
 										<svg in:fly={{x: -50, z: -10}} out:fly={{x: -50, z: -10}} style="grid-column: 1; grid-row: 1"  class="size-full block m-auto pt-3 content-center items-center place-items-center text-center" xmlns="http://www.w3.org/2000/svg" width="2.11676in" height="1.5in" viewBox="0 0 152.407 108">
 											<g>
 												<rect width="152.407" height="108" style="fill: none"/>
