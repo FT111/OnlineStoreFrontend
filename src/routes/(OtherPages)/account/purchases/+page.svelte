1fd627a119d57e9413591abbf7d7e863bf9e0bc1
FT111 <thetaylor2016@gmail.com>
2025-02-27 23:02:44 +0000

    Switches purchase page layout to visually merge orders in purchases

--- src/routes/(OtherPages)/account/purchases/+page.svelte
+++ src/routes/(OtherPages)/account/purchases/+page.svelte
@@ -41,18 +41,16 @@
 				{@const purchaseTotalValue = orders.reduce((acc, order) => acc + order.value, 0)}
 				<div class="flex flex-col rounded-xl p-4 gap-1">
 					<div class="flex flex-row justify-between px-2.5 items-center">
-						<h2 class="text-xl">Purchase made on {purchaseDate}</h2>
+						<h2 class="text-md">Purchase made on {purchaseDate} <span class="text-muted-foreground">• Arriving in {orders.length} shipment{orders.length!==1 ? 's': ''}</span></h2>
 						<Price price={purchaseTotalValue} />
 					</div>
 					
-					<div class="flex flex-col gap-0">
+					<div class="flex flex-col gap-0 border border-1 border-muted-foreground/20 rounded-2xl">
 					{#each orders as purchase, index}
 						{@const statusData = orderStatusesCancellable.find((status) => status.title === purchase.status)}
-						<Card.Root class="rounded-none border-0  {index===0 && 'rounded-t-2xl'} {index===orders.length-1 ? 'rounded-b-2xl' : 'border-b-1 border-slate-100'}  bg-muted/50">
+						<Card.Root class="rounded-none border-1  {index===0 && 'rounded-t-2xl'} {index===orders.length-1 ? 'rounded-b-2xl pb-4' : ''} ">
 							<Card.Header>
-								<div class="flex flex-row justify-between">
-									<Card.Title>Order from <span class="text-muted-foreground">{purchase.seller.username}</span></Card.Title>
-									
+								<div class="flex flex-row justify-end">
 									<div class="flex flex-row gap-1.5">
 										<div class='flex flex-row items-center p-1.5 px-2.5 text-sm rounded-full bg-{statusData.colour}'>
 											<span class={`${statusData.text ? `text-${statusData.text}` : ``}`}>{purchase.status}</span>
@@ -61,27 +59,22 @@
 								</div>
 							</Card.Header>
 							<Card.Content class="py-1">
-								<ul class="flex flex-row gap-1 flex-wrap w-full">
+								<ul class="flex flex-col gap-1 w-full">
 									{#each purchase.skus as sku}
-										<li>
-											<Tooltip.Provider>
-												<Tooltip.Root>
-													<Tooltip.Trigger>
-														<a href={`/listings/${sku.listing.id}?selected=${sku.sku.id}`}>
-														<Image src={sku.sku.images[0]} alt={sku.name} class="h-15 w-15 object-cover rounded-lg" />
-														</a>
-													</Tooltip.Trigger>
-													<Tooltip.Content>
-														<p class="font-medium">
-															<span class="font-normal text-muted-foreground">
-																{sku.listing.title}
-																•
-															</span>
-															{sku.sku.title}
-														</p>
-													</Tooltip.Content>
-												</Tooltip.Root>
-											</Tooltip.Provider>
+										<li class="flex flex-row gap-2 w-full">
+											<a href={`/listings/${sku.listing.id}?selected=${sku.sku.id}`}>
+											<Image src={sku.sku.images[0]} alt={sku.name} class="h-15 w-15 object-cover rounded-lg" />
+											</a>
+											
+											<div class="flex flex-row justify-between gap-0.5 items-center w-full">
+												<a href={`/listings/${sku.listing.id}?selected=${sku.sku.id}`}>
+													<p class="text-sm font-bold">{sku.listing.title}</p>
+													<p class="text-xs">{sku.sku.title}</p>
+													<p class="text-xs">Quantity: {sku.quantity}</p>
+												</a>
+												
+												<Price price={sku.value} />
+											</div>
 										</li>
 									{/each}
 									
