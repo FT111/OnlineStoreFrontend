4bdeb6dae23e77a5b691909641dc0d792c5d32a0
FT111 <thetaylor2016@gmail.com>
2025-03-25 18:15:44 +0000

    Added 404/Error page

--- src/routes/(OtherPages)/listings/[listingID]/+page.js
+++ src/routes/(OtherPages)/listings/[listingID]/+page.js
@@ -1,8 +1,13 @@
 import { fetchListing } from '$lib/api/listings.js';
+import { error, redirect } from '@sveltejs/kit';
 
 export const load = async ({ params }) => {
   const { listingID } = params;
   const listingData = await fetchListing(listingID);
+  if (!listingData.data) {
+    error(404, 'Listing not found');
+  }
+
   return {
     listing: {
       ...listingData.data
---
+++ src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { page } from '$app/state';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { PlugZap } from 'lucide-svelte';
+
+	const headBack = () => {
+		history.back();
+	};
+</script>
+
+<div class="flex flex-col h-screen justify-center items-center gap-6">
+	<h1 class="text-7xl font-medium flex flex-row gap-6">
+		{#if page.status === 404}
+			404 Not Found
+		{:else}
+			An error occurred
+			<PlugZap size={44} strokeWidth={1} />
+		{/if}
+	</h1>
+
+	<div class="flex flex-row gap-2.5 items-center">
+		<Button onclick={headBack}>Head back</Button>
+		<p>or</p>
+		<Button variant="secondary" href="/">Go home</Button>
+	</div>
+</div>
