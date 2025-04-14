9ad03be8be6c9097b6589d85c754b5f8970113e0
FT111 <thetaylor2016@gmail.com>
2025-04-14 22:19:23 +0100

    Fixes non-existing user causing an error rather than 404ing

--- src/routes/(OtherPages)/users/[userID]/+page.js
+++ src/routes/(OtherPages)/users/[userID]/+page.js
@@ -1,9 +1,14 @@
 
 import { getUserByID } from '$lib/api/user.js';
 import { fetchListings } from '$lib/api/listings.js';
+import { error } from '@sveltejs/kit';
 
 export const load = async ({ params }) => {
 	const user = await getUserByID(params['userID']);
+
+	if (!user.data) {
+		error(404, 'User not found');
+	}
 	return {
 		profileUser: {
 			...user.data
